255d92ed61d3fa55982bb4ce6d99077559c32409
“ <1870731037@163.com>
2020-02-03 20:29:11 +0800

    send message

--- src/main/java/com/example/logindemo/controller/indexController.java
+++ src/main/java/com/example/logindemo/controller/indexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 
@@ -33,16 +34,18 @@ public class indexController {
     public String login(){
         return "login";
     }
+//    String str = new String(request.getParameter("参数名").getBytes("iso-8859-1"), "utf-8");
 
     //注册方法
     @RequestMapping("/addregister")
-    public String register(HttpServletRequest request, Map<String,Object> map){
-        String username = request.getParameter("username");
-        String userPhone = request.getParameter("userPhone");
-        String userArea = request.getParameter("userArea");
-        String userSort = request.getParameter("userSort");
-        String password = request.getParameter("password");
-        String password2 = request.getParameter("password2");
+    public String register(HttpServletRequest request, Map<String,Object> map) throws UnsupportedEncodingException {
+
+        String username = new String(request.getParameter("username").getBytes("iso-8859-1"), "utf-8");
+        String userPhone = new String(request.getParameter("userPhone").getBytes("iso-8859-1"), "utf-8");
+        String userArea = new String(request.getParameter("userArea").getBytes("iso-8859-1"), "utf-8");
+        String userSort = new String(request.getParameter("userSort").getBytes("iso-8859-1"), "utf-8");
+        String password = new String(request.getParameter("password").getBytes("iso-8859-1"), "utf-8");
+        String password2 = new String(request.getParameter("password2").getBytes("iso-8859-1"), "utf-8");
 
         if (userArea.equals("暂无")){
             map.put("msg","请选择考生所在省份!");
